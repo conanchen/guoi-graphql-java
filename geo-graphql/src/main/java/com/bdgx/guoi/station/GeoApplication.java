30060911de649becc9c00bec479ad20bde7e917b
Conan Chen <conan8chan@yahoo.com>
2018-03-26 08:40:47 +0800

    Initial Commit

--- geo-graphql/src/main/java/com/bdgx/guoi/station/GeoApplication.java
+++ geo-graphql/src/main/java/com/bdgx/guoi/station/GeoApplication.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class GisApplication {
+public class GeoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GisApplication.class, args);
+        SpringApplication.run(GeoApplication.class, args);
     }
 
     @Bean
--- geo-graphql/src/test/java/com/bdgx/guoi/station/GeoApplicationTests.java
+++ geo-graphql/src/test/java/com/bdgx/guoi/station/GeoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GisApplicationTests {
+public class GeoApplicationTests {
 
 	@Test
 	public void contextLoads() {
