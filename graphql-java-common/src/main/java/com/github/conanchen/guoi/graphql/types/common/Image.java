caf52d4e4ba3d00c48556f37536f90e17159f739
conanchen <conan8chan@yahoo.com>
2018-04-06 17:05:57 +0800

    Split Type definition into individual graphqls file

--- graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/common/Image.java
+++ graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/common/Image.java
@@ -1,16 +1,14 @@
 package com.github.conanchen.guoi.graphql.types.common;
 
 public class Image {
-//    type Image {
-//  # A unique identifier for the image.
-private final String id;//        id: ID
-//
-//  # A word or phrase to share the nature or contents of an image.
-private final String altText;//                altText: String
-//
-//  # The location of the image as a URL.
-private final String src;//        src: URL!
-//    }
+    //  # A unique identifier for the image.
+    private final String id;//        id: ID
+
+    //  # A word or phrase to share the nature or contents of an image.
+    private final String altText;//                altText: String
+
+    //  # The location of the image as a URL.
+    private final String src;//        src: URL!
 
     public Image(String id, String altText, String src) {
         this.id = id;
