3c881d212b9dfe0fb263aa4b434a4ed2da9cc365
conanchen <conan8chan@yahoo.com>
2018-04-06 17:47:37 +0800

    Split Type definition into individual graphqls file

---
+++ graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/common/Types.java
@@ -0,0 +1,25 @@
+package com.github.conanchen.guoi.graphql.types.common;
+
+//# Information about pagination in a connection.
+//types PageInfo {
+//        # When paginating forwards, the cursor to continue.
+//        endCursor: String
+//
+//        # When paginating forwards, are there more items?
+//        hasNextPage: Boolean!
+//
+//        # When paginating backwards, are there more items?
+//        hasPreviousPage: Boolean!
+//
+//        # When paginating backwards, the cursor to continue.
+//        startCursor: String
+//}
+public class Types {
+    private Image image;//    image: Image
+    private Address address;//    address: Address
+
+    public Types(Image image, Address address) {
+        this.image = image;
+        this.address = address;
+    }
+}
