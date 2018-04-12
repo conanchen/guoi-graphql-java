d9228f8766caffcda71c03dc738a9ebe7f4b25c4
hai <hilin2333@gmail.com>
2018-04-13 07:35:20 +0800

    use builder

--- graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/attribute/Attribute.java
+++ graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/attribute/Attribute.java
@@ -1,7 +1,13 @@
 package com.github.conanchen.guoi.graphql.types.attribute;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Data
 public class Attribute {
     //      # Key or name of the attribute.
