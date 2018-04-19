52be4bd4e72e2ec728c042dcf7b171c702e17f52
linhai <hilin2333@gmail.com>
2018-04-19 14:11:14 +0800

    address input add new field

--- graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/address/AddressInput.java
+++ graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/address/AddressInput.java
@@ -6,14 +6,59 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class AddressInput {
+    //
+//            # Address line 1 (Street address/PO Box/Company name).
     private String address1;//    address1: String
+    //
+//  # Address line 2 (Apartment/Suite/Unit/Building).
     private String address2;//    address2: String
-    private String city;//    city: String
-    private String company;//    company: String
-    private String country;//    country: String
-    private String firstName;//    firstName: String
-    private String lastName;//    lastName: String
+    //
+//  # City/District/Suburb/Town/Village.
+    private String city;//            city: String
+    //
+//  # Company/Organization/Government.
+    private String company;//            company: String
+    //
+//  # State/County/Province/Region.
+    private String country;//            country: String
+    //
+//  # Two-letter country code.
+//  #
+//          # For example, US.
+    private String countryCode;//    countryCode: String
+    //
+//  # First name of the customer.
+    private String firstName;//            firstName: String
+    //    formatted(withName: Boolean = false, withCompany: Boolean = true): [String!]!
+//
+//            # Comma-separated list of city, province, and country.
+    private String formattedArea;//            formattedArea: String
+    //
+//  # Last name of the customer.
+    private String lastName;//            lastName: String
+    //
+//  # Latitude coordinate of the customer address.
+    private Float latitude;//    latitude: Float
+    //
+//  # Longitude coordinate of the customer address.
+    private Float longitude;//    longitude: Float
+    //
+//  # Name of the customer, based on first name + last name.
+    private String name;//    name: String
+    //
+//  # Unique phone number for the customer.
+//            #
+//            # Formatted using E.164 standard. For example, _+16135551111_.
     private String phone;//    phone: String
-    private String province;//    province: String
+    //
+//  # State/County/Province/Region.
+    private String province;//            province: String
+    //
+//  # Two-letter province or state code.
+//  #
+//          # For example, ON.
+    private String provinceCode;//    provinceCode: String
+    //
+//  # Zip/Postal Code.
     private String zip;//    zip: String
 }
