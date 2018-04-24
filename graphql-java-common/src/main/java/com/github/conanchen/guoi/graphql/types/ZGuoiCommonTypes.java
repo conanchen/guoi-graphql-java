91f84c075ccbcca0be48669cac2156db02e98f83
hai <hilin2333@gmail.com>
2018-04-24 16:13:20 +0800

    add attribute common type

--- graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/ZGuoiCommonTypes.java
+++ graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/ZGuoiCommonTypes.java
@@ -1,15 +1,18 @@
 package com.github.conanchen.guoi.graphql.types;
 
 import com.github.conanchen.guoi.graphql.types.address.Address;
+import com.github.conanchen.guoi.graphql.types.attribute.Attribute;
 import com.github.conanchen.guoi.graphql.types.image.Image;
 
 
 public class ZGuoiCommonTypes {
     private Image image;//    image: Image
     private Address address;//    address: Address
+    private Attribute attribute; // attribute: Attribute
 
-    public ZGuoiCommonTypes(Image image, Address address) {
+    public ZGuoiCommonTypes(Image image, Address address,Attribute attribute) {
         this.image = image;
         this.address = address;
+        this.attribute = attribute;
     }
 }
