6c7fa6f445c497c7c3ce6a5f5e238d01ea7b2ab9
hai <hilin2333@gmail.com>
2018-06-28 10:09:26 +0800

    status转user error

--- graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/util/GrpcStatusUitl.java
+++ graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/util/GrpcStatusUitl.java
@@ -1,9 +1,13 @@
 package com.github.conanchen.guoi.graphql.util;
 
+import com.github.conanchen.guoi.graphql.types.common.UserError;
 import com.google.protobuf.Any;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.rpc.BadRequest;
 import com.google.rpc.Code;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -44,5 +48,32 @@ public class GrpcStatusUitl {
                 .setCode(Code.OK.getNumber())
                 .setMessage("access success")
                 .build();
+    }
+    public static List<UserError> statusToUserErrors(com.google.rpc.Status status){
+        if (status == null || status.equals(com.google.rpc.Status.getDefaultInstance())){
+            return new ArrayList<UserError>(1){{
+                add(new UserError());
+            }};
+        }
+        List<UserError> userErrors = new ArrayList<>(1);
+        UserError userError = new UserError();
+        if (CollectionUtils.isEmpty(status.getDetailsList())) {
+            List<String> fields = new ArrayList<>(status.getDetailsList().size());
+            for (com.google.protobuf.Any any : status.getDetailsList()) {
+                try {
+                    BadRequest badRequest = any.unpack(BadRequest.class);
+                    for (BadRequest.FieldViolation fieldViolation : badRequest.getFieldViolationsList()){
+                        fields.add(String.format("filed name[%s],error msg:[%s]",
+                                fieldViolation.getField(),
+                                fieldViolation.getDescription()));
+                    }
+                }catch (InvalidProtocolBufferException e){
+                    continue;
+                }
+            }
+            userError.setField(fields);
+        }
+        userError.setMessage(status.getMessage());
+        return userErrors;
     }
 }
