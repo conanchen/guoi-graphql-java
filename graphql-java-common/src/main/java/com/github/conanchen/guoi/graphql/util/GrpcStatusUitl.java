798450fc64110deabb58057207ca1bc1aea6dbac
hai <hilin2333@gmail.com>
2018-06-28 09:15:01 +0800

    grpc返回状态工具类

---
+++ graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/util/GrpcStatusUitl.java
@@ -0,0 +1,48 @@
+package com.github.conanchen.guoi.graphql.util;
+
+import com.google.protobuf.Any;
+import com.google.rpc.BadRequest;
+import com.google.rpc.Code;
+
+import java.util.List;
+
+/**
+ * @author hai
+ * description
+ * email hilin2333@gmail.com
+ * date 2018/6/28 9:07 AM
+ */
+public class GrpcStatusUitl {
+    public static com.google.rpc.Status badRequest(List<BadRequest.FieldViolation> fieldViolations, Code code ,String message){
+        return com.google.rpc.Status.newBuilder()
+                .addDetails(Any.pack(BadRequest.newBuilder()
+                        .addAllFieldViolations(fieldViolations)
+                        .build()))
+                .setCode(code.getNumber())
+                .setMessage(message)
+                .build();
+    }
+    public static com.google.rpc.Status badRequest(BadRequest.FieldViolation fieldViolation, Code code ,String message){
+        return com.google.rpc.Status.newBuilder()
+                .addDetails(Any.pack(BadRequest.newBuilder()
+                        .addFieldViolations(fieldViolation)
+                        .build()))
+                .setCode(code.getNumber())
+                .setMessage(message)
+                .build();
+    }
+
+    public static com.google.rpc.Status ok(String message){
+        return com.google.rpc.Status.newBuilder()
+                .setCode(Code.OK.getNumber())
+                .setMessage(message)
+                .build();
+    }
+
+    public static com.google.rpc.Status ok(){
+        return com.google.rpc.Status.newBuilder()
+                .setCode(Code.OK.getNumber())
+                .setMessage("access success")
+                .build();
+    }
+}
