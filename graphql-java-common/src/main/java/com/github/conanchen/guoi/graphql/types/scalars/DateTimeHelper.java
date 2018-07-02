7f23f7fb182147c3063920db835a82a3d877ea23
hai <hilin2333@gmail.com>
2018-07-02 22:52:54 +0800

    时间序列化使用本地时区

--- graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/scalars/DateTimeHelper.java
+++ graphql-java-common/src/main/java/com/github/conanchen/guoi/graphql/types/scalars/DateTimeHelper.java
@@ -34,7 +34,7 @@ public class DateTimeHelper {
     public static String toISOString(LocalDateTime dateTime) {
         Objects.requireNonNull(dateTime, "dateTime");
 
-        return DateTimeFormatter.ISO_INSTANT.format(ZonedDateTime.of(dateTime, ZoneOffset.UTC));
+        return DateTimeFormatter.ISO_INSTANT.format(ZonedDateTime.of(dateTime, ZoneOffset.systemDefault()));
     }
 
     public static String toISOString(LocalDate date) {
